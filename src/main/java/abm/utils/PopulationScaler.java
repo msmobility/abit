be708efcb1433aeb51d2b746999b9ded97c34871
cllorca1 <carlos.llorca@tum.de>
2022-07-07 09:24:53 +0200

    placeholder for scaler of the population

---
+++ src/main/java/abm/utils/PopulationScaler.java
@@ -0,0 +1,17 @@
+package abm.utils;
+
+import abm.data.DataSet;
+import abm.properties.AbitResources;
+
+public class PopulationScaler {
+
+    private double scaleFactor = AbitResources.instance.getDouble("population.scale.factor", 1.0);
+
+
+    public void scaleDownPopulation(DataSet dataSet){
+
+
+    }
+
+
+}
