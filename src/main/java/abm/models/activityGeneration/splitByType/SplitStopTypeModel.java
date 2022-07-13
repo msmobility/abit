f0b553ad04dc1622dc458fe31b780f226904e3ae
cllorca1 <carlos.llorca@tum.de>
2022-07-13 09:23:00 +0200

    add start time probability to the split stop model

--- src/main/java/abm/models/activityGeneration/splitByType/SplitStopTypeModel.java
+++ src/main/java/abm/models/activityGeneration/splitByType/SplitStopTypeModel.java
@@ -1,59 +1,19 @@
 package abm.models.activityGeneration.splitByType;
 
-import abm.data.plans.*;
-import abm.data.timeOfDay.TimeOfWeekDistribution;
-import abm.properties.AbitResources;
-import abm.properties.InternalProperties;
-import abm.utils.AbitUtils;
+import abm.data.plans.Activity;
+import abm.data.plans.Leg;
+import abm.data.plans.StopType;
+import abm.data.plans.Tour;
 import abm.data.pop.Person;
 import abm.data.timeOfDay.AvailableTimeOfWeek;
-import de.tum.bgu.msm.util.MitoUtil;
+import abm.properties.InternalProperties;
+import abm.utils.AbitUtils;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.time.DayOfWeek;
-import java.util.HashMap;
-import java.util.Map;
 
-public class SimpleSplitStopTypeWithTimeAvailability implements SplitStopType {
+public class SplitStopTypeModel implements SplitStopType {
 
     private int counterErrors = 0;
-    private final Map<Purpose, TimeOfWeekDistribution> timeOfWeekDistributionMap;
-
-    public SimpleSplitStopTypeWithTimeAvailability() {
-        timeOfWeekDistributionMap = new HashMap<>();
-        readTimeOfDayDistributions();
-    }
-
-    private void readTimeOfDayDistributions() {
-
-        int timeIndex;
-        int purposeIndex;
-        int probabilityIndex;
-
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(AbitResources.instance.getString("start.time.distributions")));
-            String[] firstLine = br.readLine().split(",");
-
-            timeIndex = MitoUtil.findPositionInArray("time", firstLine);
-            purposeIndex = MitoUtil.findPositionInArray("purpose", firstLine);
-            probabilityIndex = MitoUtil.findPositionInArray("probability", firstLine);
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                int time = Integer.parseInt(line.split(",")[timeIndex]);
-                Purpose purpose = Purpose.valueOf(line.split(",")[purposeIndex]);
-                double probability = Double.parseDouble(line.split(",")[probabilityIndex]);
-
-                timeOfWeekDistributionMap.putIfAbsent(purpose, new TimeOfWeekDistribution());
-                timeOfWeekDistributionMap.get(purpose).setProbability(time, probability);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public StopType getStopType(Person person, Activity activity, Tour tour) {
@@ -72,11 +32,9 @@ public class SimpleSplitStopTypeWithTimeAvailability implements SplitStopType {
 
         //count periods before
         int nBefore = 0;
-        double pBefore = 0;
         for (int t = tourStart_min - InternalProperties.SEARCH_INTERVAL_MIN; t > midnight; t -= InternalProperties.SEARCH_INTERVAL_MIN) {
             if (availableTimeOfDay.isAvailable(t) == 1) {
                 nBefore++;
-                pBefore += timeOfWeekDistributionMap.get(activity.getPurpose()).probability(t);
             } else {
                 //until it finds the previous blocked time
                 break;
@@ -85,11 +43,9 @@ public class SimpleSplitStopTypeWithTimeAvailability implements SplitStopType {
 
         //count periods after
         int nAfter = 0;
-        double pAfter = 0;
         for (int t = tourEnd_min + InternalProperties.SEARCH_INTERVAL_MIN; t < midnight + 60 * 24; t += InternalProperties.SEARCH_INTERVAL_MIN) {
             if (availableTimeOfDay.isAvailable(t) == 1) {
                 nAfter++;
-                pAfter += timeOfWeekDistributionMap.get(activity.getPurpose()).probability(t);
             } else {
                 //until it finds the next blocked time
                 break;
@@ -98,7 +54,7 @@ public class SimpleSplitStopTypeWithTimeAvailability implements SplitStopType {
 
 
         if (nAfter * 15 > activity.getDuration() && nBefore * 15 > activity.getDuration()) {
-            double probabilityBefore = pBefore / (pBefore + pAfter);
+            double probabilityBefore = Double.valueOf(nBefore) / (nBefore + nAfter);
             if (AbitUtils.randomObject.nextDouble() < probabilityBefore) {
                 return StopType.BEFORE;
             } else {
