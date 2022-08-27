5ef8e8ca519ce6f729f89c8b28f95e18c0145b14
Wei <weichieh.huang@tum.de>
2022-08-27 17:48:13 +0200

    added nested logit habitual mode choice

--- src/main/java/abm/models/modeChoice/NestedLogitHabitualModeChoiceModel.java
+++ src/main/java/abm/models/modeChoice/NestedLogitHabitualModeChoiceModel.java
@@ -1,20 +1,30 @@
 package abm.models.modeChoice;
 
 import abm.data.DataSet;
+import abm.data.geo.RegioStaR2;
 import abm.data.plans.Mode;
+import abm.data.pop.Household;
 import abm.data.pop.Person;
 import abm.io.input.CoefficientsReader;
 import abm.properties.AbitResources;
+import abm.utils.AbitUtils;
+import de.tum.bgu.msm.data.person.Gender;
+import de.tum.bgu.msm.data.person.Occupation;
+import de.tum.bgu.msm.modules.modeChoice.ModeChoice;
+import de.tum.bgu.msm.util.MitoUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.nio.file.Path;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
-public class NestedLogitHabitualModeChoiceModel implements HabitualModeChoice{
-
+public class NestedLogitHabitualModeChoiceModel implements HabitualModeChoice {
+    private final static Logger logger = LogManager.getLogger(NestedLogitHabitualModeChoiceModel.class);
 
     private final DataSet dataSet;
-    private Map<Mode,Map<String, Double>> coefficients;
+    private Map<Mode, Map<String, Double>> coefficients;
 
 
     public NestedLogitHabitualModeChoiceModel(DataSet dataSet) {
@@ -23,7 +33,8 @@ public class NestedLogitHabitualModeChoiceModel implements HabitualModeChoice{
         Path pathToFile = Path.of(AbitResources.instance.getString("habitual.mode.coef"));
 
         //the following loop will read the coefficient file Mode.getModes().size() times, which is acceptable?
-        for (Mode mode : Mode.getModes()){
+        //Todo the modes in the habitual mode choice model is not consistent with the ABIT mode definition. Check with Carlos and Joanna
+        for (Mode mode : Mode.getModes()) {
             final Map<String, Double> modeCoefficients = new CoefficientsReader(dataSet, mode.toString().toLowerCase(), pathToFile).readCoefficients();
             coefficients.put(mode, modeCoefficients);
         }
@@ -34,18 +45,132 @@ public class NestedLogitHabitualModeChoiceModel implements HabitualModeChoice{
     public void chooseHabitualMode(Person person) {
 
         Map<Mode, Double> utilities = new HashMap<>();
-        for (Mode mode : Mode.getModes()){
-            utilities.put(mode, calculateUtilityForThisMode(person));
+        for (Mode mode : Mode.getModes()) {
+            utilities.put(mode, calculateUtilityForThisMode(mode, person));
         }
 
-        //do here the nested logit mode choice calculations, depending on nesting structure and coefficient
+        final double utilityAutoD = utilities.get(Mode.CAR_DRIVER);
+        final double utilityAutoP = utilities.get(Mode.CAR_PASSENGER);
+        final double utilityPT = (utilities.get(Mode.BUS) + utilities.get(Mode.TRAM_METRO) + utilities.get(Mode.TRAIN))/3;
+        final double utilityBicycle = utilities.get(Mode.BIKE);
+        final double utilityWalk = utilities.get(Mode.WALK);
+
+        final Double nestingCoefficientAutoModes = coefficients.get(Mode.CAR_DRIVER).get("nestingCoefficient");
+        final Double nestingCoefficientActiveModes = coefficients.get(Mode.BUS).get("nestingCoefficient");
+
+        double expsumNestAuto =
+                Math.exp(utilityAutoD / nestingCoefficientAutoModes) +
+                        Math.exp(utilityAutoP / nestingCoefficientAutoModes);
+
+        double expsumNestActive =
+                Math.exp(utilityBicycle / nestingCoefficientActiveModes) +
+                        Math.exp(utilityWalk / nestingCoefficientActiveModes);
+
+        double expsumTopLevel =
+                Math.exp(nestingCoefficientAutoModes * Math.log(expsumNestAuto)) +
+                        Math.exp(utilityPT) +
+                        Math.exp(nestingCoefficientActiveModes * Math.log(expsumNestActive));
+
+        double probabilityAutoD;
+        double probabilityAutoP;
+        if (expsumNestAuto > 0) {
+            probabilityAutoD =
+                    (Math.exp(utilityAutoD / nestingCoefficientAutoModes) / expsumNestAuto) * (Math.exp(nestingCoefficientAutoModes * Math.log(expsumNestAuto)) / expsumTopLevel);
+            probabilityAutoP =
+                    (Math.exp(utilityAutoP / nestingCoefficientAutoModes) / expsumNestAuto) * (Math.exp(nestingCoefficientAutoModes * Math.log(expsumNestAuto)) / expsumTopLevel);
+        } else {
+            probabilityAutoD = 0.0;
+            probabilityAutoP = 0.0;
+        }
+
+        double probabilityPT = Math.exp(utilityPT) / expsumTopLevel;
+
+        double probabilityBike;
+        double probabilityWalk;
+        if (expsumNestActive > 0) {
+            probabilityBike =
+                    (Math.exp(utilityBicycle / nestingCoefficientActiveModes) / expsumNestActive) * (Math.exp(nestingCoefficientActiveModes * Math.log(expsumNestActive)) / expsumTopLevel);
+            probabilityWalk =
+                    (Math.exp(utilityWalk / nestingCoefficientActiveModes) / expsumNestActive) * (Math.exp(nestingCoefficientActiveModes * Math.log(expsumNestActive)) / expsumTopLevel);
+        } else {
+            probabilityBike = 0.0;
+            probabilityWalk = 0.0;
+        }
+
+        EnumMap<Mode, Double> probabilities = new EnumMap<>(Mode.class);
+        probabilities.put(Mode.CAR_DRIVER, probabilityAutoD);
+        probabilities.put(Mode.CAR_PASSENGER, probabilityAutoP);
+        probabilities.put(Mode.BIKE, probabilityBike);
+        probabilities.put(Mode.WALK, probabilityWalk);
+        probabilities.put(Mode.BUS, probabilityPT); //Todo temporary assign pt to bus mode
+
+        //found Nan when there is no transit!!
+        probabilities.replaceAll((mode, probability) ->
+                probability.isNaN() ? 0 : probability);
+
+        double sum = 0;
+        for (double probability : probabilities.values()) {
+            sum += probability;
+        }
+
+        if (sum > 0) {
+            final Mode select = MitoUtil.select(probabilities, AbitUtils.getRandomObject().nextDouble());
+            person.setHabitualMode(select);
+        } else {
+            logger.error("Negative probabilities for person " + person.getId() + "'s habitual mode");
+            person.setHabitualMode(Mode.UNKNOWN);
+        }
 
         person.setHabitualMode(Mode.UNKNOWN);
 
     }
 
-    private double calculateUtilityForThisMode(Person person) {
-        return 0;
+    private double calculateUtilityForThisMode(Mode mode, Person person) {
+        Household household = person.getHousehold();
+        double utility = 0.;
+
+        utility += coefficients.get(mode).get("(Intercept)");
+
+        utility += household.getPersons().size() * coefficients.get(mode).get("hh.size");
+
+        int numAdults = (int) household.getPersons().stream().filter(p -> p.getAge() >= 18).count();
+        int numAutos = household.getNumberOfCars();
+        double autosPerAdult = numAutos / numAdults;
+
+        utility += autosPerAdult * coefficients.get(mode).get("hh.autosPerAdult");
+
+        RegioStaR2 regioStaR2 = dataSet.getZones().get(household.getLocation().getZoneId()).getRegioStaR2Type();
+        if (regioStaR2.equals(RegioStaR2.URBAN)){
+            utility += coefficients.get(mode).get("hh.urban");
+        }
+
+        if (person.getGender().equals(Gender.FEMALE)) {
+            utility += coefficients.get(mode).get("p.female");
+        }
+
+        if (person.isHasLicense()) {
+            utility += coefficients.get(mode).get("p.driversLicense");
+        }
+
+        if (person.hasBicycle()) {
+            utility += coefficients.get(mode).get("p.ownBicycle");
+        }
+
+        if (person.getOccupation().equals(Occupation.STUDENT)) {
+            utility += coefficients.get(mode).get("p.occupationStatus_Student");
+        }
+
+        if (person.getOccupation().equals(Occupation.EMPLOYED)) {
+            int travelTime = dataSet.getTravelTimes().getTravelTimeInMinutes(person.getHousehold().getLocation(), person.getJob().getLocation(), mode, person.getJob().getStartTime_min());
+            utility += travelTime * coefficients.get(mode).get("travelTime");
+        }
+
+        if (person.getOccupation().equals(Occupation.STUDENT)) {
+            int travelTime = dataSet.getTravelTimes().getTravelTimeInMinutes(person.getHousehold().getLocation(), person.getSchol().getLocation(), mode, person.getSchol().getStartTime_min());
+            utility += travelTime * coefficients.get(mode).get("travelTime");
+        }
+
+        return utility;
     }
 
 
