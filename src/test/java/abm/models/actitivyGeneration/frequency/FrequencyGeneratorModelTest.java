430612b88a45ede0d82c8c316c5a3c0f43d67ce1
cllorca1 <carlos.llorca@tum.de>
2022-08-29 11:13:46 +0200

    basic test of frequency generation

--- src/test/java/abm/models/actitivyGeneration/frequency/FrequencyGeneratorModelTest.java
+++ src/test/java/abm/models/actitivyGeneration/frequency/FrequencyGeneratorModelTest.java
@@ -1,7 +1,12 @@
 package abm.models.actitivyGeneration.frequency;
 
 import abm.data.DataSet;
+import abm.data.geo.RegioStaR2;
+import abm.data.geo.RegioStaR7;
+import abm.data.geo.RegioStaRGem5;
 import abm.data.geo.Zone;
+import abm.data.plans.Mode;
+import abm.data.plans.Plan;
 import abm.data.plans.Purpose;
 import abm.data.pop.Household;
 import abm.data.pop.Person;
@@ -9,10 +14,13 @@ import abm.data.pop.Relationship;
 import abm.models.activityGeneration.frequency.FrequencyGeneratorModel;
 
 import abm.properties.AbitResources;
+import abm.utils.PlanTools;
 import de.tum.bgu.msm.data.person.Gender;
 import de.tum.bgu.msm.data.person.Occupation;
 import junitx.framework.Assert;
 import org.junit.Test;
+
+import java.util.Map;
 
 public class FrequencyGeneratorModelTest {
 
@@ -20,10 +28,13 @@ public class FrequencyGeneratorModelTest {
     @Test
     public void test(){
 
-        AbitResources.initializeResources("PATH_TO_PROPERTIES");
+        AbitResources.initializeResources("C:/models/abit/abit.properties");
 
         DataSet dataSet = new DataSet();
         final Zone dummyZone = new Zone(1);
+        dummyZone.setRegioStaR2Type(RegioStaR2.URBAN);
+        dummyZone.setRegioStaRGem5Type(RegioStaRGem5.METROPOLIS);
+        dummyZone.setRegioStaR7Type(RegioStaR7.URBAN_METROPOLIS);
         dataSet.getZones().put(1, dummyZone);
 
 
@@ -31,12 +42,29 @@ public class FrequencyGeneratorModelTest {
 
         Person person = new Person(1, household, 36, Gender.FEMALE,
                 Relationship.married, Occupation.EMPLOYED, true, null, 10000, null);
+        household.getPersons().add(person);
+
+        Plan.initializePlan(person);
+
+        person.setHabitualMode(Mode.TRAIN);
 
         int trips;
 
-        trips = new FrequencyGeneratorModel(dataSet, Purpose.WORK).calculateNumberOfActivitiesPerWeek(person, Purpose.WORK);
+        Map<Purpose, Integer> reference = Map.of(Purpose.WORK, 5,
+                Purpose.EDUCATION, 0,
+                Purpose.OTHER, 0,
+                Purpose.ACCOMPANY, 5,
+                Purpose.RECREATION, 1,
+                Purpose.SHOPPING, 2);
 
-        Assert.assertEquals(1, trips);
+        for (Purpose purpose : Purpose.getAllPurposes()){
+            final FrequencyGeneratorModel frequencyGeneratorModel = new FrequencyGeneratorModel(dataSet, purpose);
+            trips = frequencyGeneratorModel.calculateNumberOfActivitiesPerWeek(person, purpose);
+            System.out.println(trips + " of purpose " + purpose);
+            Assert.assertEquals((int) reference.get(purpose), trips);
+        }
+
+
 
     }
 
