f31972f0f8eb8a3ada47eb6ec31c431ccb6ac87a
cai_y <yangqian.cai@tum.de>
2023-07-06 17:40:04 +0200

    no

--- src/main/java/abm/models/PlanGenerator.java
+++ src/main/java/abm/models/PlanGenerator.java
@@ -170,7 +170,6 @@ public class PlanGenerator implements Callable {
 
 
         stopsOnDiscretionaryTours.forEach(activity -> {
-
             Tour selectedTour = planTools.findDiscretionaryTour(plan);
             activity.setDayOfWeek(selectedTour.getMainActivity().getDayOfWeek());
             timeAssignment.assignDurationToStop(activity);
