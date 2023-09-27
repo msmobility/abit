09884bcd43fd34eeffc3b38bedaa8c6d2e5d5244
Wei <weichieh.huang@tum.de>
2023-09-27 10:46:38 +0200

    added code to print people have no out-of-home acts in the act output

--- src/main/java/abm/io/output/ActivityPrinter.java
+++ src/main/java/abm/io/output/ActivityPrinter.java
@@ -2,11 +2,15 @@ package abm.io.output;
 
 import abm.data.DataSet;
 import abm.data.plans.Activity;
+import abm.data.plans.Purpose;
 import abm.data.plans.Tour;
 import abm.data.pop.Person;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.time.DayOfWeek;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivityPrinter {
 
@@ -25,14 +29,47 @@ public class ActivityPrinter {
 
 
         for (Person person : dataSet.getPersons().values()) {
-            if (person.getPlan() != null){
-                for (Tour tour : person.getPlan().getTours().values()) {
-                    for (Activity activity : tour.getActivities().values()) {
-                        pw.println(activity.toString());
+
+            if (person.getHousehold().getSimulated()){
+
+                if (person.getPlan() != null){
+
+                    if (person.getPlan().getTours().size() == 0){
+
+                        for (DayOfWeek day : DayOfWeek.values()){
+
+                            Activity homeAct = new Activity(person, Purpose.HOME);
+                            homeAct.setStartTime_min(day.ordinal() * 60 * 24);
+                            homeAct.setEndTime_min((day.ordinal()+1) * 60 * 24 -1);
+                            homeAct.setLocation(person.getHousehold().getLocation());
+                            homeAct.setDayOfWeek(day);
+
+                            pw.println(homeAct);
+                        }
+
+
+                    }else{
+
+                        Map<DayOfWeek, Boolean> dayHasTour = new HashMap<>();
+
+
+
+
+                        for (Tour tour : person.getPlan().getTours().values()) {
+                            for (Activity activity : tour.getActivities().values()) {
+                                pw.println(activity.toString());
+                            }
+
+                        }
                     }
 
+
                 }
+            }else{
+
             }
+
+
         }
 
         pw.close();
