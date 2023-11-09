d2b7db7ca5351512c40f4ef6c4e7dfd4c0e16984
cai_y <yangqian.cai@tum.de>
2023-11-09 11:07:28 +0100

    modify the selectStopDestination method (also parameters)

--- src/main/java/abm/models/destinationChoice/DestinationChoice.java
+++ src/main/java/abm/models/destinationChoice/DestinationChoice.java
@@ -2,6 +2,7 @@ package abm.models.destinationChoice;
 
 import abm.data.geo.Location;
 import abm.data.plans.Activity;
+import abm.data.plans.Tour;
 import abm.data.pop.Person;
 
 public interface DestinationChoice {
@@ -18,13 +19,17 @@ public interface DestinationChoice {
      * Selects the destination of a stop based on home and on the location of
      * the previous and the following activity
      * @param person
-     * @param previousActivity
+     * @param tour
+//     * @param previousActivity
      * @param activity
-     * @param followingActivity
+//     * @param followingActivity
      * @return
      */
-    void selectStopDestination(Person person, Activity previousActivity,
-                                   Activity activity, Activity followingActivity);
+//    void selectStopDestination(Person person, Activity previousActivity,
+//                                   Activity activity, Activity followingActivity);
+
+    void selectStopDestination(Person person, Tour tour
+                               ,Activity activity);
 
 
 }
--- src/main/java/abm/models/destinationChoice/DestinationChoiceModel.java
+++ src/main/java/abm/models/destinationChoice/DestinationChoiceModel.java
@@ -6,8 +6,7 @@ import abm.data.geo.Zone;
 import abm.data.plans.Activity;
 import abm.data.plans.Mode;
 import abm.data.plans.Purpose;
-import abm.data.pop.Household;
-import abm.data.pop.Job;
+import abm.data.plans.Tour;
 import abm.data.pop.Person;
 import abm.io.input.CoefficientsReader;
 import abm.properties.AbitResources;
@@ -17,58 +16,89 @@ import de.tum.bgu.msm.util.matrices.IndexedDoubleMatrix1D;
 import de.tum.bgu.msm.util.matrices.IndexedDoubleMatrix2D;
 import org.apache.log4j.Logger;
 import org.locationtech.jts.geom.Coordinate;
-import org.matsim.core.utils.collections.Tuple;
 
 import java.nio.file.Path;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class  DestinationChoiceModel implements DestinationChoice {
+public class DestinationChoiceModel implements DestinationChoice {
 
     private final DataSet dataSet;
     private final static Logger logger = Logger.getLogger(DestinationChoiceModel.class);
     private final Map<Purpose, Map<Zone, Double>> zoneAttractorsByPurpose;
-    private Map<Purpose, IndexedDoubleMatrix2D> utilityMatrixByPurpose = new HashMap<>();
+    private Map<Purpose, IndexedDoubleMatrix2D> probabilityMatrixByPurpose = new HashMap<>();
 
-    private static final double BETA = -0.01;
-    private static final double ALPHA = 20;
-    //TODO. Beta and alpha by purpose
+    private Map<Purpose, Map<String, Double>> coefficientsMain;
+    private Map<Purpose, Map<String, Double>> coefficientsStop;
+    private boolean runCalibrationMain = false;
+    private boolean runCalibrationStop = false;
+    private Map<Purpose, Map<String, Double>> updatedCalibrationFactorsMain = new HashMap<>();
+    private Map<Purpose, Map<String, Double>> updatedCalibrationFactorsStop = new HashMap<>();
+
+    private static final double BETAFIXED = -0.01;
+//    private static final double ALPHA = 20;
+//    //TODO. Beta and alpha by purpose
 
     public DestinationChoiceModel(DataSet dataSet) {
         this.dataSet = dataSet;
         zoneAttractorsByPurpose = loadBasicAttraction();
-        utilityMatrixByPurpose = loadUtilities();
+        probabilityMatrixByPurpose = calculateProbability();
+        this.coefficientsMain = new HashMap<>();
+        Path pathToCoefficientsMainFile = Path.of(AbitResources.instance.getString("destination.choice.main.act"));
+        this.coefficientsStop = new HashMap<>();
+        Path pathToCoefficientsStopFile = Path.of(AbitResources.instance.getString("destination.choice.stop.act"));
+
+        for (Purpose purpose : Purpose.values()) {
+            if (!(purpose.equals(Purpose.HOME) || purpose.equals(Purpose.SUBTOUR))) {
+                final Map<String, Double> purposeCoefficientsMain = new CoefficientsReader(dataSet, purpose.toString(), pathToCoefficientsMainFile).readCoefficients();
+                coefficientsMain.put(purpose, purposeCoefficientsMain);
+                final Map<String, Double> purposeCoefficientsStop = new CoefficientsReader(dataSet, purpose.toString(), pathToCoefficientsStopFile).readCoefficients();
+                coefficientsStop.put(purpose, purposeCoefficientsStop);
+            }
+        }
     }
 
-    private Map<Purpose, IndexedDoubleMatrix2D> loadUtilities() {
+    public DestinationChoiceModel(DataSet dataSet, Boolean runCalibrationMain, Boolean runCalibrationStop) {
+        this(dataSet);
+        this.updatedCalibrationFactorsMain = new HashMap<>();
+        this.updatedCalibrationFactorsStop = new HashMap<>();
+        for (Purpose purpose : Purpose.values()) {
+            if (!(purpose.equals(Purpose.HOME) || purpose.equals(Purpose.SUBTOUR))) {
+                if (runCalibrationMain.equals(true) && runCalibrationStop.equals(false)) {
+                    this.updatedCalibrationFactorsMain.putIfAbsent(purpose, new HashMap<>());
+                    updatedCalibrationFactorsMain.get(purpose).put("ALPHA", 0.);
+                    updatedCalibrationFactorsMain.get(purpose).put("BETA", 0.);
+                }
+                if (runCalibrationStop.equals(true) && runCalibrationMain.equals(false)) {
+                    this.updatedCalibrationFactorsStop.putIfAbsent(purpose, new HashMap<>());
+                    updatedCalibrationFactorsStop.get(purpose).put("ALPHA", 0.);
+                    updatedCalibrationFactorsStop.get(purpose).put("BETA", 0.);
+                }
+            }
+        }
+        this.runCalibrationMain = runCalibrationMain;
+        this.runCalibrationStop = runCalibrationStop;
+    }
 
-        Map<Purpose, Double> betaAdjustment = new HashMap<>();
-        //beta adjustment copied from MITO
-        betaAdjustment.put(Purpose.WORK,0.545653257377378); //HBW
-        betaAdjustment.put(Purpose.EDUCATION, 1.09211334287783); //HBE
-        betaAdjustment.put(Purpose.SHOPPING, 1.382831732); //HBS
-        betaAdjustment.put(Purpose.OTHER, 1.02679034779653); //HBO
-        betaAdjustment.put(Purpose.ACCOMPANY, 1.09211334287783); //same as education
-        betaAdjustment.put(Purpose.RECREATION, 0.874195571671594); //HBR
-        betaAdjustment.put(Purpose.HOME,0.545653257377378); //HBW, does not matter now
-        betaAdjustment.put(Purpose.SUBTOUR,0.733731103853844); //NHBW, does not matter now
+    private Map<Purpose, IndexedDoubleMatrix2D> calculateProbability() {
+
 
         Map<Purpose, IndexedDoubleMatrix2D> matrixByPurpose = new HashMap<>();
         for (Purpose purpose : Purpose.getAllPurposes()) {
-            IndexedDoubleMatrix2D utilityMatrix = new IndexedDoubleMatrix2D(dataSet.getZones().values(), dataSet.getZones().values());
+            IndexedDoubleMatrix2D probabilityMatrix = new IndexedDoubleMatrix2D(dataSet.getZones().values(), dataSet.getZones().values());
             for (Zone origin : dataSet.getZones().values()) {
                 for (Zone destination : dataSet.getZones().values()) {
                     final int travelDistanceInMeters = dataSet.getTravelDistances().getTravelDistanceInMeters(origin, destination, Mode.UNKNOWN, 0.);
                     final double travelDistanceInKm = travelDistanceInMeters / 1000;
                     final double attractor = zoneAttractorsByPurpose.get(purpose).get(destination);
-                    double utility = attractor * Math.exp(ALPHA * Math.exp(BETA * betaAdjustment.get(purpose) * travelDistanceInKm));
-                    utilityMatrix.setIndexed(origin.getId(), destination.getId(), utility);
+                    double utility = (coefficientsMain.get(purpose).get("ALPHA") + coefficientsMain.get(purpose).get("ALPHA_calibration")) * Math.exp(BETAFIXED * (coefficientsMain.get(purpose).get("BETA") + coefficientsMain.get(purpose).get("BETA_calibration")) * travelDistanceInKm);
+
+                    double probability = attractor * Math.exp(utility);
+                    probabilityMatrix.setIndexed(origin.getId(), destination.getId(), probability);
+
                 }
             }
-            matrixByPurpose.put(purpose, utilityMatrix);
+            matrixByPurpose.put(purpose, probabilityMatrix);
         }
-
 
         return matrixByPurpose;
     }
@@ -81,7 +111,7 @@ public class  DestinationChoiceModel implements DestinationChoice {
             String columnName = purpose.toString().toLowerCase(); //todo review this
             Map<String, Double> coefficients = new CoefficientsReader(dataSet, columnName, pathToFilePurpose).readCoefficients();
             Map<Zone, Double> attractorByZone = new HashMap<>();
-            for (Zone zone: dataSet.getZones().values()) {
+            for (Zone zone : dataSet.getZones().values()) {
                 Double value = 0.;
                 for (Map.Entry<String, Double> entry : coefficients.entrySet()) {
                     value += coefficients.get(entry.getKey()) * Integer.valueOf(String.valueOf(zone.getAttribute(entry.getKey()).get()));
@@ -93,15 +123,18 @@ public class  DestinationChoiceModel implements DestinationChoice {
         return zoneAttractorsByPurpose;
     }
 
+    public void updateMainDestinationProbability(){
+        probabilityMatrixByPurpose = calculateProbability();
+    }
 
     @Override
     public void selectMainActivityDestination(Person person, Activity activity) {
 
         Zone origin = dataSet.getZones().get(person.getHousehold().getLocation().getZoneId());
 
-        final int selectedIndex = MitoUtil.select(utilityMatrixByPurpose.get(activity.getPurpose()).viewRow(origin.getId()).toNonIndexedArray());
+        final int selectedIndex = MitoUtil.select(probabilityMatrixByPurpose.get(activity.getPurpose()).viewRow(origin.getId()).toNonIndexedArray());
 
-        final int[] columnLookupArray = utilityMatrixByPurpose.get(activity.getPurpose()).getColumnLookupArray();
+        final int[] columnLookupArray = probabilityMatrixByPurpose.get(activity.getPurpose()).getColumnLookupArray();
 
         Zone destination = dataSet.getZones().get(columnLookupArray[selectedIndex]);
 
@@ -111,33 +144,138 @@ public class  DestinationChoiceModel implements DestinationChoice {
         activity.setLocation(microDestination);
 
     }
+
 
     @Override
-    public void selectStopDestination(Person person, Activity previousActivity, Activity activity, Activity followingActivity) {
+    public void selectStopDestination(Person person, Tour tour, Activity stopActivity) {
+        Zone homeZone = dataSet.getZones().get(person.getHousehold().getLocation().getZoneId());
+        Zone mainActZone = dataSet.getZones().get(tour.getMainActivity().getLocation().getZoneId());
+        Purpose mainActPurpose = tour.getMainActivity().getPurpose();
 
-        Zone previousActivityZone = dataSet.getZones().get(previousActivity.getLocation().getZoneId());
-        Zone followingActivityZone = dataSet.getZones().get(followingActivity.getLocation().getZoneId());
+        if (mainActPurpose.equals(Purpose.WORK)) {
+            final IndexedDoubleMatrix1D workStopUtilityList = new IndexedDoubleMatrix1D(dataSet.getZones().values());
+            for (Zone z : dataSet.getZones().values()) {
+                final int travelDistanceInMeters = dataSet.getTravelDistances().getTravelDistanceInMeters(homeZone, z, Mode.UNKNOWN, 0.);
+                final double travelDistanceInKm = travelDistanceInMeters / 1000;
+                final double attractor = zoneAttractorsByPurpose.get(stopActivity.getPurpose()).get(z);
+                double workStopUtility = attractor * Math.exp((coefficientsStop.get(mainActPurpose).get("ALPHA") + coefficientsStop.get(mainActPurpose).get("ALPHA_calibration")) * Math.exp((coefficientsStop.get(mainActPurpose).get("BETA") + coefficientsStop.get(mainActPurpose).get("BETA_calibration")) * travelDistanceInKm));
+                workStopUtilityList.setIndexed(z.getId(), workStopUtility);
+            }
+            final int selectedIndex = MitoUtil.select(workStopUtilityList.toNonIndexedArray());
+            Zone destination = dataSet.getZones().get(selectedIndex);
 
-        final IndexedDoubleMatrix1D utilityfromPrevious = utilityMatrixByPurpose.get(activity.getPurpose()).viewRow(previousActivityZone.getId());
-        final IndexedDoubleMatrix1D utiliyToFollowing = utilityMatrixByPurpose.get(activity.getPurpose()).viewRow(followingActivityZone.getId());
-
-        final IndexedDoubleMatrix1D jointUtility = new IndexedDoubleMatrix1D(dataSet.getZones().values());
-
-        for (Zone z : dataSet.getZones().values()) {
-            jointUtility.setIndexed(z.getId(), utilityfromPrevious.getIndexed(z.getId()) * utiliyToFollowing.getIndexed(z.getId()));
+            final Coordinate randomCoordinate = destination.getRandomCoordinate(AbitUtils.getRandomObject());
+            MicroscopicLocation microDestination = new MicroscopicLocation(randomCoordinate.x, randomCoordinate.y);
+            microDestination.setZone(destination);
+            stopActivity.setLocation(microDestination);
         }
 
-        final int selectedIndex = MitoUtil.select(jointUtility.toNonIndexedArray());
+        if (mainActPurpose.equals(Purpose.EDUCATION) || mainActPurpose.equals(Purpose.ACCOMPANY) || mainActPurpose.equals(Purpose.SHOPPING) || mainActPurpose.equals(Purpose.OTHER) || mainActPurpose.equals(Purpose.RECREATION)) {
+            final IndexedDoubleMatrix1D othersStopUtilityList = new IndexedDoubleMatrix1D(dataSet.getZones().values());
+            for (Zone z : dataSet.getZones().values()) {
+                final int travelDistanceInMeters = dataSet.getTravelDistances().getTravelDistanceInMeters(mainActZone, z, Mode.UNKNOWN, 0.);
+                final double travelDistanceInKm = travelDistanceInMeters / 1000;
+                final double attractor = zoneAttractorsByPurpose.get(stopActivity.getPurpose()).get(z);
+                double otherStopUtility = attractor * Math.exp((coefficientsStop.get(mainActPurpose).get("ALPHA") + coefficientsStop.get(mainActPurpose).get("ALPHA_calibration")) * Math.exp((coefficientsStop.get(mainActPurpose).get("BETA") + coefficientsStop.get(mainActPurpose).get("BETA_calibration")) * travelDistanceInKm));
+                othersStopUtilityList.setIndexed(z.getId(), otherStopUtility);
+            }
+            final int selectedIndex = MitoUtil.select(othersStopUtilityList.toNonIndexedArray());
+            Zone destination = dataSet.getZones().get(selectedIndex);
 
-        final int[] columnLookupArray = utilityMatrixByPurpose.get(activity.getPurpose()).getColumnLookupArray();
-
-        Zone destination = dataSet.getZones().get(columnLookupArray[selectedIndex]);
-
-        final Coordinate randomCoordinate = destination.getRandomCoordinate(AbitUtils.getRandomObject());
-        MicroscopicLocation microDestination = new MicroscopicLocation(randomCoordinate.x, randomCoordinate.y);
-        microDestination.setZone(destination);
-        activity.setLocation(microDestination);
-
+            final Coordinate randomCoordinate = destination.getRandomCoordinate(AbitUtils.getRandomObject());
+            MicroscopicLocation microDestination = new MicroscopicLocation(randomCoordinate.x, randomCoordinate.y);
+            microDestination.setZone(destination);
+            stopActivity.setLocation(microDestination);
+        }
 
     }
+
+
+    //Old codes
+//    @Override
+//    public void selectStopDestination(Person person, Activity previousActivity, Activity activity, Activity followingActivity) {
+//
+//        Zone previousActivityZone = dataSet.getZones().get(previousActivity.getLocation().getZoneId());
+//        Zone followingActivityZone = dataSet.getZones().get(followingActivity.getLocation().getZoneId());
+//
+//        final IndexedDoubleMatrix1D utilityfromPrevious = utilityMatrixByPurpose.get(activity.getPurpose()).viewRow(previousActivityZone.getId());
+//        final IndexedDoubleMatrix1D utiliyToFollowing = utilityMatrixByPurpose.get(activity.getPurpose()).viewRow(followingActivityZone.getId());
+//
+//        final IndexedDoubleMatrix1D jointUtility = new IndexedDoubleMatrix1D(dataSet.getZones().values());
+//
+//        for (Zone z : dataSet.getZones().values()) {
+//            jointUtility.setIndexed(z.getId(), utilityfromPrevious.getIndexed(z.getId()) * utiliyToFollowing.getIndexed(z.getId()));
+//        }
+//
+//        final int selectedIndex = MitoUtil.select(jointUtility.toNonIndexedArray());
+//
+//        final int[] columnLookupArray = utilityMatrixByPurpose.get(activity.getPurpose()).getColumnLookupArray();
+//
+//        Zone destination = dataSet.getZones().get(columnLookupArray[selectedIndex]);
+//
+//        final Coordinate randomCoordinate = destination.getRandomCoordinate(AbitUtils.getRandomObject());
+//        MicroscopicLocation microDestination = new MicroscopicLocation(randomCoordinate.x, randomCoordinate.y);
+//        microDestination.setZone(destination);
+//        activity.setLocation(microDestination);
+//
+//
+//    }
+
+    public void updateCalibrationFactorsMain(Map<Purpose, Map<String, Double>> newCalibrationFactorsMain) {
+        for (Purpose purpose : Purpose.values()) {
+            if (!(purpose.equals(Purpose.HOME) || purpose.equals(Purpose.SUBTOUR))) {
+                double calibrationFactorsFromLastIteration = this.updatedCalibrationFactorsMain.get(purpose).get("BETA_calibration");
+                double updatedCalibrationFactors = newCalibrationFactorsMain.get(purpose).get("BETA_calibration");
+                this.updatedCalibrationFactorsMain.get(purpose).replace("BETA_calibration", calibrationFactorsFromLastIteration + updatedCalibrationFactors);
+                logger.info("Calibration factor for " + purpose + "\t" + ": " + updatedCalibrationFactorsMain);
+            }
+        }
+
+    }
+
+    public void updateCalibrationFactorsStop(Map<Purpose, Map<String, Double>> newCalibrationFactorsStop) {
+        for (Purpose purpose : Purpose.values()) {
+            if (!(purpose.equals(Purpose.HOME) || purpose.equals(Purpose.SUBTOUR))) {
+                double calibrationFactorsFromLastIteration = this.updatedCalibrationFactorsStop.get(purpose).get("BETA");
+                double updatedCalibrationFactors = newCalibrationFactorsStop.get(purpose).get("BETA");
+                this.updatedCalibrationFactorsStop.get(purpose).replace("BETA", calibrationFactorsFromLastIteration + updatedCalibrationFactors);
+                logger.info("Calibration factor for " + purpose + "\t" + ": " + updatedCalibrationFactorsStop);
+            }
+        }
+
+    }
+
+    public Map<Purpose, Map<String, Double>> obtainCoefficientsTableMain() {
+        double originalCalibrationFactor = 0.0;
+        double updatedCalibrationFactor = 0.0;
+        double latestCalibrationFactor = 0.0;
+
+        for (Purpose purpose : Purpose.values()) {
+            if (!(purpose.equals(Purpose.HOME) || purpose.equals(Purpose.SUBTOUR))) {
+                originalCalibrationFactor = coefficientsMain.get(purpose).get("BETA_calibration");
+                updatedCalibrationFactor = updatedCalibrationFactorsMain.get(purpose).get("BETA_calibration");
+                latestCalibrationFactor = originalCalibrationFactor + updatedCalibrationFactor;
+                coefficientsMain.get(purpose).replace("BETA_calibration", latestCalibrationFactor);
+            }
+        }
+        return this.coefficientsMain;
+    }
+
+    public Map<Purpose, Map<String, Double>> obtainCoefficientsTableStop() {
+        double originalCalibrationFactor = 0.0;
+        double updatedCalibrationFactor = 0.0;
+        double latestCalibrationFactor = 0.0;
+
+        for (Purpose purpose : Purpose.values()) {
+            if (!(purpose.equals(Purpose.HOME) || purpose.equals(Purpose.SUBTOUR))) {
+                originalCalibrationFactor = coefficientsStop.get(purpose).get("BETA_calibration");
+                updatedCalibrationFactor = updatedCalibrationFactorsStop.get(purpose).get("BETA_calibration");
+                latestCalibrationFactor = originalCalibrationFactor + updatedCalibrationFactor;
+                coefficientsStop.get(purpose).replace("BETA_calibration", latestCalibrationFactor);
+            }
+        }
+        return this.coefficientsStop;
+    }
+
+
 }
