7454cd03daecafa7496d35f32caa960c1949c927
Wei <weichieh.huang@tum.de>
2023-12-07 14:32:56 +0100

    added run partition

--- src/main/java/abm/RunAbit.java
+++ src/main/java/abm/RunAbit.java
@@ -5,8 +5,10 @@ import abm.data.DataSet;
 import abm.data.plans.Mode;
 import abm.data.plans.Purpose;
 import abm.data.pop.Household;
-import abm.data.pop.Person;
 import abm.io.input.DefaultDataReaderManager;
+import abm.io.output.*;
+import abm.models.ModelSetup;
+import abm.models.ModelSetupMuc;
 import abm.io.output.OutputWriter;
 import abm.models.*;
 import abm.properties.AbitResources;
@@ -15,6 +17,7 @@ import de.tum.bgu.msm.util.MitoUtil;
 import de.tum.bgu.msm.util.concurrent.ConcurrentExecutor;
 import org.apache.log4j.Logger;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -53,13 +56,24 @@ public class RunAbit {
         long start = System.currentTimeMillis();
 
         //TODO: parallelize by household not person because of vehicle assignment. Later, for joint travel/coordination destination, need to move parallelization into model steps?
-        for (Household household : dataSet.getHouseholds().values()) {
-            if (AbitUtils.getRandomObject().nextDouble() < AbitResources.instance.getDouble("scale.factor", 1.0)) {
-                final int i = AbitUtils.getRandomObject().nextInt(threads);
-                householdsByThread.putIfAbsent(i, new ArrayList<>());
-                householdsByThread.get(i).add(household);
+        if (AbitResources.instance.getDouble("scale.factor", 1.0) >= 1){
+            for (Household household : dataSet.getHouseholds().values()) {
+                if (household.getPartition() == 2){
+                    final int i = AbitUtils.getRandomObject().nextInt(threads);
+                    householdsByThread.putIfAbsent(i, new ArrayList<>());
+                    householdsByThread.get(i).add(household);
+                    household.setSimulated(Boolean.TRUE);
+                }
             }
-
+        }else {
+            for (Household household : dataSet.getHouseholds().values()) {
+                if (AbitUtils.getRandomObject().nextDouble() < AbitResources.instance.getDouble("scale.factor", 1.0)) {
+                    final int i = AbitUtils.getRandomObject().nextInt(threads);
+                    householdsByThread.putIfAbsent(i, new ArrayList<>());
+                    householdsByThread.get(i).add(household);
+                    household.setSimulated(Boolean.TRUE);
+                }
+            }
         }
 
         for (int i = 0; i < threads; i++) {
@@ -68,7 +82,29 @@ public class RunAbit {
 
         executor.execute();
 
-        // TODO: new code
+        if (Boolean.parseBoolean(AbitResources.instance.getString("model.calibration"))){
+            CalibrationMuc calibrationMuc = new CalibrationMuc(dataSet);
+            calibrationMuc.runCalibration();
+        }
+
+        //todo. summary (trip length frequency distribution, etc.)
+        String outputFolder = AbitResources.instance.getString("base.directory") + "/output/";
+
+        logger.info("Printing out results");
+        try {
+
+            new StatisticsPrinter(dataSet).print(outputFolder + "/distanceDistribution.csv");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        //todo. consistency check before summarizing the trip list (useful for the debugging phase, have it as false for the application later on)
+
+        //todo. auto calibration outputs
+
+        // TODO: new code - Yangqian
         CheckResults checkResults = new CheckResults(dataSet);
         checkResults.checkTimeConflict();
         System.out.println("Number of people with schedule conflict: "+checkResults.getNumOfPeopleWithTimeConflict());
@@ -84,8 +120,9 @@ public class RunAbit {
             System.out.println("Number of child trip for "+purpose+" without the accompany in the household:"+checkResults.getChildTripWithoutAccompany().get(purpose));
         }
 
-        // TODO: new code
         long end = System.currentTimeMillis();
+
+
 
         long time = (end - start)/1000;
 
