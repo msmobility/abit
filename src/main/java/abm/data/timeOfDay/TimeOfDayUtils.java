2d8f8385bec343c4b2be01fde8bff2a97e28131a
Wei <weichieh.huang@tum.de>
2023-12-07 14:30:23 +0100

    added new time assignment approach

--- src/main/java/abm/data/timeOfDay/TimeOfDayUtils.java
+++ src/main/java/abm/data/timeOfDay/TimeOfDayUtils.java
@@ -11,12 +11,13 @@ public class TimeOfDayUtils {
 
     /**
      * Converts the departure time distribution to a modified version of it where non-available time windows have a probability of zero
+     *
      * @param originalTOD
      * @param availableTOD
      * @return
      */
     public static TimeOfWeekDistribution updateTODWithAvailability(TimeOfWeekDistribution originalTOD,
-                                                                  AvailableTimeOfWeek availableTOD) {
+                                                                   AvailableTimeOfWeek availableTOD) {
 
         TimeOfWeekDistribution newTOD = new TimeOfWeekDistribution();
         for (int minute : originalTOD.getMinutes()) {
@@ -35,10 +36,32 @@ public class TimeOfDayUtils {
         return newTOD;
     }
 
+    public static TimeOfWeekDistribution updateTODWithAvailabilityAndDuration(TimeOfWeekDistribution originalTOD,
+                                                                              BlockedTimeOfWeekLinkedList blockedTOD,
+                                                                              int duration) {
+
+        TimeOfWeekDistribution newTOD = new TimeOfWeekDistribution();
+        for (int minute : originalTOD.getMinutes()) {
+            boolean runThrough = true;
+            for (int endMinute = minute; endMinute <= minute + duration; endMinute += InternalProperties.SEARCH_INTERVAL_MIN) {
+                if (blockedTOD.isAvailable(endMinute) == 0) {
+                    newTOD.setProbability(minute, 0);
+                    runThrough = false;
+                    break;
+                }
+            }
+            if (runThrough) {
+                newTOD.setProbability(minute, originalTOD.probability(minute));
+            }
+        }
+        return newTOD;
+    }
+
 
     /**
      * Converts the available time object to a new one that can accomodate a trip of certain duration. It avoids starting a trip that will overlap
      * with an already existing one
+     *
      * @param baseAvailableTOD
      * @param tripDuration
      * @return
@@ -49,7 +72,7 @@ public class TimeOfDayUtils {
         AvailableTimeOfWeek newAvailableTOD = new AvailableTimeOfWeek();
 
         for (int minute = InternalProperties.SEARCH_INTERVAL_MIN; minute < MAX_VALUE; minute = minute + InternalProperties.SEARCH_INTERVAL_MIN) {
-            if (baseAvailableTOD.isAvailable(minute) == 0 && baseAvailableTOD.isAvailable(minute - InternalProperties.SEARCH_INTERVAL_MIN) == 1){
+            if (baseAvailableTOD.isAvailable(minute) == 0 && baseAvailableTOD.isAvailable(minute - InternalProperties.SEARCH_INTERVAL_MIN) == 1) {
                 newAvailableTOD.blockTime(Math.max(0, minute - tripDuration), minute);
             } else if (baseAvailableTOD.isAvailable(minute) == 0) {
                 newAvailableTOD.blockTime(minute - InternalProperties.SEARCH_INTERVAL_MIN, minute);
@@ -64,7 +87,7 @@ public class TimeOfDayUtils {
         BlockedTimeOfWeekLinkedList newBlockedTOD = new BlockedTimeOfWeekLinkedList();
 
         for (int minute = InternalProperties.SEARCH_INTERVAL_MIN; minute < MAX_VALUE; minute = minute + InternalProperties.SEARCH_INTERVAL_MIN) {
-            if (baseBlockedTOD.isAvailable(minute) == 0 && baseBlockedTOD.isAvailable(minute - InternalProperties.SEARCH_INTERVAL_MIN) == 1){
+            if (baseBlockedTOD.isAvailable(minute) == 0 && baseBlockedTOD.isAvailable(minute - InternalProperties.SEARCH_INTERVAL_MIN) == 1) {
                 newBlockedTOD.blockTime(Math.max(0, minute - tripDuration), minute);
             } else if (baseBlockedTOD.isAvailable(minute) == 0) {
                 newBlockedTOD.blockTime(minute - InternalProperties.SEARCH_INTERVAL_MIN, minute);
