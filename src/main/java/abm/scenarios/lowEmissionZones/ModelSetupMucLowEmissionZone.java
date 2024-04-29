4d06351b294a3fc53f744e6f961157d304e83c51
Wei <weichieh.huang@tum.de>
2024-04-29 22:45:41 +0200

    added a new model setup for low emission zone scenario

---
+++ src/main/java/abm/scenarios/lowEmissionZones/ModelSetupMucLowEmissionZone.java
@@ -0,0 +1,135 @@
+package abm.scenarios.lowEmissionZones;
+
+import abm.data.DataSet;
+import abm.data.plans.Purpose;
+import abm.io.input.BikeOwnershipReader;
+import abm.models.ModelSetup;
+import abm.models.activityGeneration.frequency.FrequencyGenerator;
+import abm.models.activityGeneration.frequency.FrequencyGeneratorModel;
+import abm.models.activityGeneration.frequency.SubtourGenerator;
+import abm.models.activityGeneration.frequency.SubtourGeneratorModel;
+import abm.models.activityGeneration.splitByType.SplitByType;
+import abm.models.activityGeneration.splitByType.SplitByTypeModel;
+import abm.models.activityGeneration.splitByType.SplitStopByTypeModel;
+import abm.models.activityGeneration.splitByType.SplitStopType;
+import abm.models.activityGeneration.time.*;
+import abm.models.destinationChoice.*;
+import abm.models.modeChoice.*;
+import abm.scenarios.lowEmissionZones.model.McLogsumBasedDestinationChoiceModel;
+import org.apache.commons.collections.map.HashedMap;
+
+import java.util.Map;
+
+public class ModelSetupMucLowEmissionZone implements ModelSetup {
+
+    private final Map<Purpose, FrequencyGenerator> frequencyGenerators;
+    private final HabitualModeChoice habitualModeChoice;
+    private final DestinationChoice destinationChoice;
+    private final TourModeChoice tourModeChoice;
+    private final DayOfWeekMandatoryAssignment dayOfWeekMandatoryAssignment;
+    private final DayOfWeekDiscretionaryAssignment dayOfWeekDiscretionaryAssignment;
+    private final TimeAssignment timeAssignment;
+    private final SplitByType splitByType;
+    private final SplitStopType stopSplitType;
+    private final SubtourGenerator subtourGenerator;
+    private final SubtourTimeAssignment subtourTimeAssignment;
+    private final SubtourDestinationChoice subtourDestinationChoice;
+    private final SubtourModeChoice subtourModeChoice;
+
+    public BikeOwnershipReader getBikeOwnershipReader() {
+        return bikeOwnershipReader;
+    }
+
+    private final BikeOwnershipReader bikeOwnershipReader;
+
+
+    public ModelSetupMucLowEmissionZone(DataSet dataSet) {
+
+        bikeOwnershipReader = new BikeOwnershipReader(dataSet);
+        dayOfWeekMandatoryAssignment = new DayOfWeekMandatoryAssignmentModel(dataSet);
+        tourModeChoice = new NestedLogitTourModeChoiceModel(dataSet);
+        habitualModeChoice = new NestedLogitHabitualModeChoiceModel(dataSet);
+        dayOfWeekDiscretionaryAssignment = new DayOfWeekDiscretionaryAssignmentModel(dataSet);
+
+        frequencyGenerators = new HashedMap();
+        for (Purpose purpose : Purpose.getAllPurposes()){
+            frequencyGenerators.put(purpose, new FrequencyGeneratorModel(dataSet, purpose));
+        }
+        stopSplitType = new SplitStopByTypeModel();
+        splitByType = new SplitByTypeModel(dataSet);
+        destinationChoice = new McLogsumBasedDestinationChoiceModel(dataSet);
+        timeAssignment = new TimeAssignmentModel(dataSet);
+
+        subtourGenerator = new SubtourGeneratorModel(dataSet);
+        subtourTimeAssignment = new SubtourTimeAssignmentModel(dataSet);
+        subtourDestinationChoice  =new SubtourDestinationChoiceModel(dataSet);
+        subtourModeChoice = new SubtourModeChoiceModel(dataSet);
+
+    }
+
+    @Override
+    public HabitualModeChoice getHabitualModeChoice() {
+        return habitualModeChoice;
+    }
+
+    @Override
+    public Map<Purpose, FrequencyGenerator> getFrequencyGenerator() {
+        return frequencyGenerators;
+    }
+
+    @Override
+    public DestinationChoice getDestinationChoice() {
+        return destinationChoice;
+    }
+
+    @Override
+    public TourModeChoice getTourModeChoice() {
+        return tourModeChoice;
+    }
+
+    @Override
+    public DayOfWeekMandatoryAssignment getDayOfWeekMandatoryAssignment() {
+        return dayOfWeekMandatoryAssignment;
+    }
+
+    @Override
+    public DayOfWeekDiscretionaryAssignment getDayOfWeekDiscretionaryAssignment() {
+        return dayOfWeekDiscretionaryAssignment;
+    }
+
+    @Override
+    public TimeAssignment getTimeAssignment() {
+        return timeAssignment;
+    }
+
+    @Override
+    public SplitByType getSplitByType() {
+        return splitByType;
+    }
+
+    @Override
+    public SplitStopType getStopSplitType() {
+        return stopSplitType;
+    }
+
+    @Override
+    public SubtourGenerator getSubtourGenerator() {
+        return subtourGenerator;
+    }
+
+    @Override
+    public SubtourTimeAssignment getSubtourTimeAssignment() {
+        return subtourTimeAssignment;
+    }
+
+    @Override
+    public SubtourDestinationChoice getSubtourDestinationChoice() {
+        return subtourDestinationChoice;
+    }
+
+    @Override
+    public SubtourModeChoice getSubtourModeChoice() {
+        return subtourModeChoice;
+    }
+
+}
